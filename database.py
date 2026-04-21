db3aaf76aadb840c31200f64aa7107bc0b5886d2
pabloalonsomartineza <pabloalonsomartineza@gmail.com>
2026-04-21 20:32:01 +1000

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 
-from app.config import settings
+from config import settings
 
 engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
